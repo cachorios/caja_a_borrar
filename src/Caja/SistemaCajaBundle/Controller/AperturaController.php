c7c76c11b94a4f8d562dfa8a925976dca9239679
CachoRios <cachorios@gmail.com>
2013-04-23 19:03:00 -0300

    serie prn

--- src/Caja/SistemaCajaBundle/Controller/AperturaController.php
+++ src/Caja/SistemaCajaBundle/Controller/AperturaController.php
@@ -575,10 +575,61 @@ class AperturaController extends Controller implements IModuloAuditable {
         return $response->setContent($rJson);
     }
 
+
     /**
      * @return Array, un array con los nombres de los actions excluidos
      */
     function getNoAuditables() {
         return Array('deleteAction', 'editAction');
+    }
+
+    public function sockAction()
+    {
+
+        $fp = fsockopen ("192.0.4.183", 5331, $errno, $errstr, 30);
+        if (!$fp) {
+            echo "$errstr ($errno)";
+        } else {
+            $e = chr(27);
+            //fputs ($fp, chr(149) );
+            echo "Inicio: $fp";
+            fputs ($fp, chr(hexdec ('H1B')) . "a"  . chr(1) );
+            fputs ($fp, chr(hexdec ('H1B')) . "c0" . chr(2) );
+            fputs ($fp, chr(hexdec ('H1B')) . "U"  . chr(0) );
+            fputs ($fp, chr(hexdec ('H1B')) . "!"  . chr(2) ); //tama�o
+            fputs ($fp, chr(hexdec ('H1B')) . "M"  . chr(2) );
+
+            fputs ($fp, "PRUEBA de IMPRESION"  . chr(hexdec ('HA')) );
+
+            fputs ($fp, chr(hexdec ('H1B')) . "!"  . chr(1) ); //Tama�o normal
+            fputs ($fp, chr(hexdec ('H1B')) . "M"  . chr(1) );
+
+
+            fputs ($fp, chr(hexdec ('H1B')) . "c0" . chr(3) ); //Enviar al journal
+            fputs ($fp, chr(hexdec ('H1B')) . "z" . chr(1) );
+            fputs ($fp, chr(hexdec ('H1B')) . "!" . chr(1) );
+
+            fputs ($fp, "TIKET: 121545 "  . chr(hexdec ('HA')) );
+            fputs ($fp, "****************************************"  . chr(hexdec ('HA')) );
+
+            fputs ($fp, chr(hexdec ('H1B')) . "a" . chr(0) );
+
+
+            /*
+            for($i=1; $i<10; $i++){
+                $string = 'string text here '.$i."\n<br>";
+                echo $string;
+                fputs ($fp, $string );
+            }		*/
+
+            fclose ($fp);
+
+            $resp = new Response();
+
+            $resp->setContent("Ver");
+            return $resp;
+
+        }
+
     }
 }
